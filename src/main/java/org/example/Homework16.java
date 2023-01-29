3bc08046bc6e432b715aa22c0f920751cc1b7870
KamilaKenzhibayeva <kamila.kenzhibayeva@testpro.io>
2023-01-29 12:07:05 -0500

    HomeWork16

--- src/main/java/org/example/Homework16.java
+++
@@ -1,7 +0,0 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
-import java.time.Duration;
-

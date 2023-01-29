be4dadbc4481e8cbcdb65d72fc2ef53e41d13e9b
Kostiantyn <konstantin.pavlishin@testpro.io>
2023-01-28 21:24:21 -0600

    Update LoginTests.java

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -17,5 +19,20 @@ public class LoginTests extends BaseTest {
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
+    }
+    @Test
+    public static void RegistrationTest(){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+        WebElement registerButton = driver.findElement(By.cssSelector("#hel"));
+        registerButton.click();
+        String regurl = "https://bbb.testpro.io/registration.php";
+        driver.get(regurl);
+        Assert.assertEquals(driver.getCurrentUrl(),regurl);
+        driver.quit();
+
     }
 }

4a715122879265b923772c981f1d2983bd8542c7
goldcluster_LK <123644292+goldclusterLK@users.noreply.github.com>
2023-01-29 21:46:21 -0600

    HomeWork#16

---
+++ src/test/java/Homework16.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+import java.time.Duration;
+
+public class Homework16 extends BaseTest{
+
+    @Test
+    public static void RegistrationNavigationTest(){
+         WebDriver currentDriver= new ChromeDriver();
+           currentDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+           String url="https://bbb.testpro.io/";
+           currentDriver.get(url);
+
+           WebElement registrationInput = currentDriver.findElement(By.cssSelector("a[id='hel'])"));
+           registrationInput.click();
+           WebElement registerButton = currentDriver.findElement(By.cssSelector("input[value='Register']"));
+           Assert.assertTrue(registerButton.isDisplayed());
+           currentDriver.quit();
+
+
+    }
+}

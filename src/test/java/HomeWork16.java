6116f214a31047a698e7eee5b30919c1b6f0be2e
KamilaKenzhibayeva <kamila.kenzhibayeva@testpro.io>
2023-01-29 19:50:39 -0500

    HomeWork16

---
+++ src/test/java/HomeWork16.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.time.Duration;
+public class HomeWork16 extends BaseTest {
+    @Test
+    public static void RegistrationNavigation() {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+        WebElement registrationInput = driver.findElement(By.cssSelector("[id='hel']"));
+        registrationInput.click();
+        WebElement registration = driver.findElement(By.cssSelector("[value='Register']"));
+        Assert.assertTrue(registration.isDisplayed());
+        driver.quit();
+    }
+}
+
+

ee14bd82167661b11056a6dede81a60f401e1fe5
DashaTagirs <123782563+DashaTagirs@users.noreply.github.com>
2023-01-31 17:49:44 -0600

    added annotations

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,15 +4,32 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import java.time.Duration;
+
 
 public class BaseTest {
+    Webdriver driver;
 
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
+    @BeforeMethod
+    public void setUpBrowser(){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+     @AfterMethod
+    public void tearDown() {
+        driver.quit();
+     }
+
 
 }
+
+

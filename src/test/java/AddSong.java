4d951908b3063e5d37bafc24d961b3e29b2e8f1e
Kostiantyn <konstantin.pavlishin@testpro.io>
2023-01-31 11:50:31 -0600

    Update AddSong.java

--- src/test/java/AddSong.java
+++ src/test/java/AddSong.java
@@ -3,7 +3,8 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.openqa.selenium.interactions.Actions;
-public class AddSong extends BaseTest{
+
+public class AddSong extends BaseTest {
     @Test
     public void addSongToPlaylist() {
         enterEmail("rumkostia92@gmail.com");
@@ -14,17 +15,18 @@ public class AddSong extends BaseTest{
 
     }
 
-        public void addSongToAlbum(){
-            Actions action = new Actions(driver);
-            WebElement song = driver.findElement(By.cssSelector("ol[class='top-song-list'] > li:nth-child(3) > article > span[class='cover']"));
-            action.contextClick(song).perform();
-            WebElement addButton = driver.findElement(By.cssSelector("[class='has-sub']"));
-            addButton.click();
-            WebElement menuAlbum = driver.findElement(By.xpath("//ul[@class='menu submenu menu-add-to']/li[contains(text(),'ZXC234')]"));
-            menuAlbum.click();
+    public void addSongToAlbum() {
+        Actions action = new Actions(driver);
+        WebElement song = driver.findElement(By.cssSelector("ol[class='top-song-list'] > li:nth-child(3) > article > span[class='cover']"));
+        action.contextClick(song).perform();
+        WebElement addButton = driver.findElement(By.cssSelector("[class='has-sub']"));
+        addButton.click();
+        WebElement menuAlbum = driver.findElement(By.xpath("//ul[@class='menu submenu menu-add-to']/li[contains(text(),'ZXC234')]"));
+        menuAlbum.click();
 
-        }
-    public void checkSongInAlbum(){
+    }
+
+    public void checkSongInAlbum() {
         WebElement homeAlbum = driver.findElement(By.cssSelector("[href='#!/playlist/33111']"));
         homeAlbum.click();
         WebElement albumSong = driver.findElement(By.xpath("//tr/td[contains(text(),'Tunnel of Lights (ID 1689)')]"));

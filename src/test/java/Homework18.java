743d669ba4087abdb73c852307bd703820047092
Kostiantyn <konstantin.pavlishin@testpro.io>
2023-02-02 19:19:56 -0600

    Create Homework18.java

---
+++ src/test/java/Homework18.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+public class Homework18 extends BaseTest{
+    @Test
+    public void playSong(){
+        enterEmail("rumkostia92@gmail.com");
+        enterPassword("te$t$tudent");
+        loginSubmit();
+        
+    }
+
+}

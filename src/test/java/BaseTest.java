ce39b85aba9a99fa7b817d044bb78092fbd4edc8
Kostiantyn <konstantin.pavlishin@testpro.io>
2023-02-02 19:19:49 -0600

    Update BaseTest.java

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,14 +1,14 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
 import java.time.Duration;
+import java.util.UUID;
 
 
 public class BaseTest {
@@ -32,5 +32,54 @@ public class BaseTest {
     @AfterMethod
     public void tearDown() {
         driver.quit();
+    }
+
+
+
+    protected void openUserProfile() {
+        WebElement profile = driver.findElement(By.cssSelector(".view-profile"));
+        profile.click();
+    }
+
+
+
+
+    public void setName(String newName) {
+        WebElement nameInput = driver.findElement(By.cssSelector("#inputProfileName"));
+        nameInput.click();
+        nameInput.clear();
+        nameInput.sendKeys(newName);
+    }
+
+    public void setPassword() {
+        WebElement passwordInput = driver.findElement(By.cssSelector("#inputProfileCurrentPassword"));
+        passwordInput.click();
+        passwordInput.sendKeys("te$t$tudent");
+    }
+
+    public void saveProfile() {
+        WebElement saveButton = driver.findElement(By.cssSelector(".btn-submit"));
+        saveButton.click();
+    }
+
+    public String generateRandomName() {
+        return UUID.randomUUID().toString().replace("-", "");//
+    }
+
+    public void enterEmail(String email) {
+        WebElement emailInput = driver.findElement(By.cssSelector("[type='email']"));
+        emailInput.click();
+        emailInput.sendKeys(email);
+    }
+
+    public void enterPassword(String password) {
+        WebElement passwordInput = driver.findElement(By.cssSelector("[type='password']"));
+        passwordInput.click();
+        passwordInput.sendKeys(password);
+    }
+
+    public void loginSubmit() {
+        WebElement submitLogin = driver.findElement(By.cssSelector("button[type='submit']"));
+        submitLogin.click();
     }
 }

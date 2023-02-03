754481775e5a3608f31a8530e47cbf96fe5a26e0
DESKTOP-3D86B1I\Ksenia Strizhkova <ksenia.strigkova@gmail.com>
2023-02-02 21:49:35 -0600

    Homework17

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,7 +1,9 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.AfterMethod;
@@ -9,6 +11,8 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
 import java.time.Duration;
+
+import static java.lang.Thread.*;
 
 
 public class BaseTest {
@@ -24,13 +28,36 @@ public class BaseTest {
 
     @BeforeMethod
     public void setUpBrowser() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get(url);
+        driver.manage().window().maximize();
     }
 
     @AfterMethod
     public void tearDown() {
         driver.quit();
     }
+
+    public static void enterEmail(String email) {
+        WebElement emailInput = driver.findElement(By.cssSelector("[type='email']"));
+        emailInput.click();
+        emailInput.sendKeys(email);
+    }
+
+    public static void enterPassword(String password) {
+        WebElement passwordInput = driver.findElement(By.cssSelector("[type='password']"));
+        passwordInput.click();
+        passwordInput.sendKeys(password);
+    }
+
+    public static void loginSubmit() {
+        WebElement submitLogin = driver.findElement(By.cssSelector("button[type='submit']"));
+        submitLogin.click();
+    }
+
+
+
 }
---
+++ src/test/java/Homework17.java
@@ -0,0 +1,55 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+
+public class Homework17 extends BaseTest{
+    @Test
+    public static void addSongToPlaylist()  {
+
+        enterEmail("ksenia.strigkova@gmail.com");
+        enterPassword("te$t$tudent");
+        loginSubmit();
+        clickAllSongs();
+        chooseSong();
+        clickAddTo();
+        checkSongInAlbum();
+
+
+
+    }
+    public static void clickAllSongs() {
+        WebElement clickAllSongs = driver.findElement(By.cssSelector("a[href='#!/songs']"));
+        clickAllSongs.click();
+
+    }
+    private static void chooseSong() {
+        WebElement song = driver.findElement(By.cssSelector(".all-songs .song-item:nth-of-type(5) .title"));
+        song.click();
+
+    }
+
+    private static void clickAddTo() {
+        WebElement addToButton = driver.findElement(By.cssSelector(".btn-add-to"));
+        addToButton.click();
+        WebElement addToPlaylist = driver.findElement(By.cssSelector("section#songsWrapper .existing-playlists > ul > .playlist"));
+        addToPlaylist.click();
+
+    }
+
+
+    public static void checkSongInAlbum() {
+        WebElement myPlaylist = driver.findElement(By.xpath("//section[@id='playlists']/ul/li[3]"));
+        myPlaylist.click();
+        WebElement songcheck = driver.findElement(By.cssSelector(".all-songs .song-item:nth-of-type(5) .title"));
+        songcheck.isDisplayed();
+    }
+
+
+}
+
+

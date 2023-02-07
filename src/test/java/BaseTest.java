185f5b4a1bd6ed273896bb96b2088510733db243
Kostiantyn <konstantin.pavlishin@testpro.io>
2023-02-07 15:15:13 -0600

    Update BaseTest.java

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
@@ -16,6 +18,7 @@ import java.util.UUID;
 public class BaseTest {
 
     static WebDriver driver;
+   static WebDriverWait wait;
     static String url;
 
 
@@ -27,11 +30,12 @@ public class BaseTest {
 
     @BeforeMethod
     @Parameters({"BaseURL"})
-    public void setUpBrowser(String BaseURL) {
+    public void setUpBrowser(String BaseURL) throws InterruptedException {
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        wait = new WebDriverWait(driver,Duration.ofSeconds(10));
         String url=BaseURL;
         driver.get(url);
+
     }
 
     @AfterMethod
@@ -42,7 +46,7 @@ public class BaseTest {
 
 
     protected void openUserProfile() {
-        WebElement profile = driver.findElement(By.cssSelector(".view-profile"));
+        WebElement profile = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".view-profile")));
         profile.click();
     }
 
@@ -50,20 +54,20 @@ public class BaseTest {
 
 
     public void setName(String newName) {
-        WebElement nameInput = driver.findElement(By.cssSelector("#inputProfileName"));
+        WebElement nameInput = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#inputProfileName")));
         nameInput.click();
         nameInput.clear();
         nameInput.sendKeys(newName);
     }
 
     public void setPassword() {
-        WebElement passwordInput = driver.findElement(By.cssSelector("#inputProfileCurrentPassword"));
+        WebElement passwordInput = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#inputProfileCurrentPassword")));
         passwordInput.click();
         passwordInput.sendKeys("te$t$tudent");
     }
 
     public void saveProfile() {
-        WebElement saveButton = driver.findElement(By.cssSelector(".btn-submit"));
+        WebElement saveButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".btn-submit")));
         saveButton.click();
     }
 
@@ -72,19 +76,19 @@ public class BaseTest {
     }
 
     public void enterEmail(String email) {
-        WebElement emailInput = driver.findElement(By.cssSelector("[type='email']"));
+        WebElement emailInput = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[type='email']")));
         emailInput.click();
         emailInput.sendKeys(email);
     }
 
     public void enterPassword(String password) {
-        WebElement passwordInput = driver.findElement(By.cssSelector("[type='password']"));
+        WebElement passwordInput = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[type='password']")));
         passwordInput.click();
         passwordInput.sendKeys(password);
     }
 
     public void loginSubmit() {
-        WebElement submitLogin = driver.findElement(By.cssSelector("button[type='submit']"));
+        WebElement submitLogin = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[type='submit']")));
         submitLogin.click();
     }
 }

127fa3d3c2b778777ce2eb974de8a6ff8343f92a
DESKTOP-3D86B1I\Ksenia Strizhkova <ksenia.strigkova@gmail.com>
2023-02-09 13:57:57 -0600

    Homework21

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,18 +1,73 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import java.time.Duration;
+
+import static java.lang.Thread.*;
+
 
 public class BaseTest {
+
+    static WebDriver driver;
+    static WebDriverWait wait;
+    static String url = "https://bbb.testpro.io/";
+    static String urlHome = "https://bbb.testpro.io/#!/home";
+
 
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
 
+    @BeforeMethod
+    public void setUpBrowser() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(4));
+        driver.get(url);
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+    public static void enterEmail(String email) {
+        WebElement emailInput = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[type='email']")));
+        emailInput.click();
+        emailInput.sendKeys(email);
+    }
+
+    public static void enterPassword(String password) {
+        WebElement passwordInput = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(" [type='password']")));
+        passwordInput.click();
+        passwordInput.sendKeys(password);
+    }
+
+    public static void loginSubmit() {
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(" button[type='submit']"))).click();
+    }
+
+    public static void login(String email, String password) {
+        WebElement emailInput = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[type='email']")));
+        emailInput.click();
+        emailInput.sendKeys(email);
+        WebElement passwordInput = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(" [type='password']")));
+        passwordInput.click();
+        passwordInput.sendKeys(password);
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(" button[type='submit']"))).click();
+    }
 }
---
+++ src/test/java/Homework21.java
@@ -0,0 +1,62 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.Test;
+
+public class Homework21 extends BaseTest {
+
+
+    @Test
+    public static void createPlaylist() {
+        login("ksenia.strigkova@gmail.com", "te$t$tudent");
+        addPlaylist();
+        selectPlaylist();
+        enterNamePlaylist();
+        notificationText();
+    }
+
+    @Test
+    public static void renamePlaylist() {
+
+        login("ksenia.strigkova@gmail.com", "te$t$tudent");
+        doubleClickOnPlaylist();
+        enterNewNamePlaylist("blabla22");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("blabla22")));
+    }
+
+    public static void addPlaylist() {
+        WebElement btnAddPlaylist = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".fa-plus-circle")));
+        btnAddPlaylist.click();
+    }
+
+    public static void selectPlaylist() {
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[data-testid='playlist-context-menu-create-simple']"))).click();
+    }
+
+    public static void enterNamePlaylist() {
+        WebElement inputName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[name='name']")));
+        inputName.click();
+        inputName.clear();
+        inputName.sendKeys("lalala666");
+        inputName.sendKeys(Keys.ENTER);
+    }
+
+    public static void notificationText() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".success")));
+    }
+
+    private static void doubleClickOnPlaylist() {
+        WebElement playlist = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("lalala666")));
+        new Actions(driver)
+                .doubleClick(playlist)
+                .perform();
+    }
+
+    public static void enterNewNamePlaylist(String name) {
+        WebElement inputPlaylistName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[data-testid='inline-playlist-name-input']")));
+        inputPlaylistName.sendKeys(Keys.HOME, Keys.chord(Keys.SHIFT, Keys.END), name);
+        inputPlaylistName.sendKeys(Keys.ENTER);
+    }
+}

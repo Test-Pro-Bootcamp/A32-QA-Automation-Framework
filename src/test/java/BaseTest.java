a156a2c0061087c8d4a8616cb6a458d7158b2e87
Kostiantyn <konstantin.pavlishin@testpro.io>
2023-02-09 14:02:54 -0600

    Update BaseTest.java

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -10,19 +10,14 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
-
 import java.time.Duration;
-import java.util.UUID;
+
 
 
 public class BaseTest {
 
     static WebDriver driver;
    static WebDriverWait wait;
-    static String url;
-
-
-
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
@@ -37,58 +32,24 @@ public class BaseTest {
         driver.get(url);
 
     }
-
     @AfterMethod
     public void tearDown() {
         driver.quit();
     }
-
-
-
-    protected void openUserProfile() {
-        WebElement profile = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".view-profile")));
-        profile.click();
-    }
-
-
-
-
-    public void setName(String newName) {
-        WebElement nameInput = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#inputProfileName")));
-        nameInput.click();
-        nameInput.clear();
-        nameInput.sendKeys(newName);
-    }
-
-    public void setPassword() {
-        WebElement passwordInput = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#inputProfileCurrentPassword")));
-        passwordInput.click();
-        passwordInput.sendKeys("te$t$tudent");
-    }
-
-    public void saveProfile() {
-        WebElement saveButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".btn-submit")));
-        saveButton.click();
-    }
-
-    public String generateRandomName() {
-        return UUID.randomUUID().toString().replace("-", "");//
-    }
-
     public void enterEmail(String email) {
         WebElement emailInput = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[type='email']")));
         emailInput.click();
         emailInput.sendKeys(email);
     }
-
     public void enterPassword(String password) {
         WebElement passwordInput = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[type='password']")));
         passwordInput.click();
         passwordInput.sendKeys(password);
     }
-
     public void loginSubmit() {
         WebElement submitLogin = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[type='submit']")));
         submitLogin.click();
     }
 }
+//        Email("demo@class.com");
+//        Password("te$t$tudent");

2f938851091cd4fb81ac23cf33d258f16e95a3b3
DanilKrivulin <123902343+DanilKrivulin@users.noreply.github.com>
2023-02-10 20:51:51 -0500

    HOMEWORK20

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,16 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.opentelemetry.sdk.trace.internal.data.ExceptionEventData;
+import org.checkerframework.checker.units.qual.K;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
@@ -30,7 +35,7 @@ public class BaseTest {
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         wait = new WebDriverWait(driver, Duration.ofSeconds(4));
-       // driver.manage().window().maximize();
+        driver.manage().window().maximize();
         driver.get(url);
     }
 
@@ -56,9 +61,34 @@ public class BaseTest {
         submitLogin.click();
     }
 
-    public void login(String email, String password){
+    public void logIn(String email, String password){
         enterEmail(email);
         enterPassword(password);
         loginSubmit();
+    }
+    public void createPlaylist() {
+        WebElement createplaylist = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[data-testid='sidebar-create-playlist-btn']")));
+        createplaylist.click();
+    }
+    public void Playlistselect() {
+        WebElement playlistselect = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[data-testid='playlist-context-menu-create-simple']")));
+        playlistselect.click();
+    }
+    public void  enterName() {
+        WebElement nameplaylist = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@name='name']")));
+        nameplaylist.sendKeys("Shine");
+        nameplaylist.sendKeys(Keys.ENTER);
+    }
+    public void openPlaylist() {
+        WebElement playlistList = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Shine']")));
+        playlistList.click();
+    }
+    public void deletePlaylist() {
+        WebElement deletePlaylistbutton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[class='del btn-delete-playlist']")));
+        deletePlaylistbutton.click();
+    }
+    public void successMessage() {
+        WebElement successMsg = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[class='success show']")));
+        Assert.assertTrue(successMsg.isDisplayed());
     }
 }

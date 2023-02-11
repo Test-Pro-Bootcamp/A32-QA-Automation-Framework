731cb6c5af99e964b9922c83940bcc1ba91e79d2
JuliaBurkovaQA <julia.burkova@testpro.io>
2023-02-11 13:41:12 +0300

    Homework 21

--- src/test/java/HomeWork21.java
+++ src/test/java/HomeWork21.java
@@ -9,26 +9,46 @@ import static org.openqa.selenium.Keys.*;
 
 public class HomeWork21 extends BaseTest {
     @Test
+    //double click
     public void renamePlaylist() {
-        login("burkovads@mail.ru","Julka@0721");
+        login("burkovads@mail.ru", "Julka@0721");
         userPlaylist();
-        enterNameOfPlaylist();
+        enterNameOfPlaylist("By DoubleClick");
         succeedRenamePlaylist();
     }
-
     public void userPlaylist() {
         Actions action = new Actions(driver);
         WebElement userPlaylist = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".playlist:nth-child(3)>a")));
         action.doubleClick(userPlaylist).perform();
     }
-    public void enterNameOfPlaylist() {
+    public void enterNameOfPlaylist(String newName) {
         WebElement inputField = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input[name='name']")));
         inputField.sendKeys(CONTROL, "a", BACK_SPACE);
-        inputField.sendKeys("New Name");
+        inputField.sendKeys(newName);
         inputField.sendKeys(ENTER);
     }
+
     public void succeedRenamePlaylist() {
         WebElement successBaner = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".success")));
         Assert.assertTrue(successBaner.isDisplayed());
+    }
+
+    //click Edit
+    @Test
+    public void renamePlaylistByEdit() {
+        login("burkovads@mail.ru", "Julka@0721");
+        userPlaylistRightClick();
+        clickEdit();
+        enterNameOfPlaylist("By Edit");
+        succeedRenamePlaylist();
+    }
+    public void userPlaylistRightClick () {
+        Actions action = new Actions(driver);
+        WebElement userPlaylist = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".playlist:nth-child(3)>a")));
+        action.contextClick(userPlaylist).perform();
+    }
+    public void clickEdit() {
+        WebElement edit = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".menu.playlist-item-menu > ul > li:nth-of-type(1)")));
+        edit.click();
     }
 }

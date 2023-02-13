3806795eb2898737704a7cd0b1c1d4e5c8d23eea
Kostiantyn <konstantin.pavlishin@testpro.io>
2023-02-12 21:47:58 -0600

    Update PlaylistPage.java

--- src/test/java/pages/PlaylistPage.java
+++ src/test/java/pages/PlaylistPage.java
@@ -1,7 +1,14 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class PlaylistPage extends BasePage{
-    By deleteButtonLocator = By.cssSelector("button[title='Delete this playlist']");
+    private final By deleteButtonLocator = By.cssSelector("button[title='Delete this playlist']");
+    public WebElement getDeleteButton(){
+        return wait.until(ExpectedConditions.elementToBeClickable(deleteButtonLocator));
+
+    }
+
 }

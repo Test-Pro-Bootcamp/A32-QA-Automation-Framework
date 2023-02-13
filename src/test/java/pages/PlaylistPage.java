7280abc803d341f57daab67df0e8565a4fc76aed
Kostiantyn <konstantin.pavlishin@testpro.io>
2023-02-12 20:57:24 -0600

    Create PlaylistPage.java

---
+++ src/test/java/pages/PlaylistPage.java
@@ -0,0 +1,7 @@
+package pages;
+
+import org.openqa.selenium.By;
+
+public class PlaylistPage extends BasePage{
+    By deleteButtonLocator = By.cssSelector("button[title='Delete this playlist']");
+}

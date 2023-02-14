06b97c96f98da474e124aa5cc8c2e887cf652cbe
Kostiantyn <konstantin.pavlishin@testpro.io>
2023-02-14 15:27:41 -0600

    Update BasePage.java

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
@@ -16,39 +17,20 @@ import org.testng.annotations.Parameters;
 import java.time.Duration;
 
 public class BasePage {
-    protected static WebDriver driver;
-    protected static WebDriverWait wait;
-    protected static Actions actions;
-    @BeforeSuite
-    static void setupClass() {
-        WebDriverManager.chromedriver().setup();
+     public static WebDriver driver;
+     public static WebDriverWait wait;
+     public static Actions actions;
+    public BasePage(WebDriver setDriver){
+        driver=setDriver;
+        actions=new Actions(driver);
+        wait= new WebDriverWait(driver,Duration.ofSeconds(5));
+        PageFactory.initElements(driver,this);
+    }
+    public void doubleClick(WebElement element){
+        actions.doubleClick(element).perform();
     }
 
-    @BeforeMethod
-    @Parameters({"BaseURL"})
-    public void setUpBrowser(String BaseURL)  {
-        driver = new ChromeDriver();
-        wait = new WebDriverWait(driver,Duration.ofSeconds(10));
-        actions = new Actions(driver);
-        String url = BaseURL;
-        driver.get(url);
-    }
-    @AfterMethod
-    public void tearDown() {
-        driver.quit();
-    }
-    public WebElement findElement(By locator){
-        return wait.until(ExpectedConditions.elementToBeClickable(locator));
-    }
-    public void doubleClick(By locator){
-        actions.doubleClick(findElement(locator)).perform();
-    }
-    public void click(By locator){
-        actions.click(findElement(locator)).perform();
-    }
-    public void sendKeys(By locator,String text){
-        actions.sendKeys(findElement(locator),text);
-    }
+
 }
 
 

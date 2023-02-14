d6024ffb2022ea959f66f88bf550fec5aaea3cdb
Kostiantyn <konstantin.pavlishin@testpro.io>
2023-02-14 15:27:27 -0600

    Update HomePage.java

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -2,45 +2,59 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 
 public class HomePage extends BasePage {
-    private final By NotificationLocator = By.cssSelector("[class='success show']");
-    private final By playlistLocator = By.xpath("//li[@class='playlist playlist']");
-    private final By playlistFieldLocator = By.cssSelector("[data-testid='inline-playlist-name-input']");
-    private final By avatarLocator = By.cssSelector(".avatar");
-    private final By createPlaylistBtnLocator = By.cssSelector("[title='Create a new playlist']");
-    private final By newPlaylistBtnLocator = By.cssSelector("[data-testid='playlist-context-menu-create-simple']");
-    private final By newPlaylistFieldLocator= By.cssSelector("input[name='name']");
-    public void enterPlaylist() {
+    @FindBy(css = "[class='success show']" )
+    WebElement NotificationLocator;
+    @FindBy(css ="[data-testid='inline-playlist-name-input']")
+    WebElement playlistFieldLocator;
+    @FindBy(xpath = "//li[@class='playlist playlist']")
+    WebElement playlistLocator;
+    @FindBy(css = ".avatar")
+    WebElement avatarLocator;
+    @FindBy(css ="[title='Create a new playlist']")
+    WebElement createPlaylistBtnLocator;
+    @FindBy(css ="[data-testid='playlist-context-menu-create-simple']" )
+    WebElement newPlaylistBtnLocator;
+    @FindBy(css ="input[name='name']")
+    WebElement newPlaylistFieldLocator;
+    public HomePage(WebDriver setDriver) {
+        super(setDriver);
+    }
+    public HomePage enterPlaylist() {
         doubleClick(playlistLocator);
-
+        return this;
     }
-    public void changePlaylistName(String playlist) {
-
-        WebElement playlistField = driver.findElement(playlistFieldLocator);
-        playlistField.sendKeys(Keys.CONTROL + "a");
-        playlistField.sendKeys(Keys.DELETE);
-        playlistField.sendKeys(playlist, Keys.ENTER);
+    public HomePage changePlaylistName(String playlist) {
+        playlistFieldLocator.sendKeys(Keys.CONTROL + "a");
+        playlistFieldLocator.sendKeys(Keys.DELETE);
+        playlistFieldLocator.sendKeys(playlist, Keys.ENTER);
+        return this;
     }
-    public void deleteActions(){
-        PlaylistPage playlistPage = new PlaylistPage();
-        click(playlistLocator);
-        click(playlistPage.getDeleteButton());
+    public HomePage deleteActions(){
+        PlaylistPage playlistPage = new PlaylistPage(driver);
+        playlistLocator.click();
+        playlistPage.getDeleteButton().click();
+        return this;
     }
     public WebElement getNotification(){
-        return wait.until(ExpectedConditions.presenceOfElementLocated(NotificationLocator));
+        return NotificationLocator;
     }
     public WebElement getAvatar(){
-        return wait.until(ExpectedConditions.presenceOfElementLocated(avatarLocator));
+        return avatarLocator;
     }
-    public void createNewPlaylist(){
-        click(createPlaylistBtnLocator);
-        click(newPlaylistBtnLocator);
+    public HomePage createNewPlaylist(){
+        createPlaylistBtnLocator.click();
+        newPlaylistBtnLocator.click();
+        return this;
     }
-    public void setPlaylistName(String name){
-        driver.findElement(newPlaylistFieldLocator).sendKeys(name,Keys.ENTER);
+    public HomePage setPlaylistName(String name){
+        newPlaylistFieldLocator.sendKeys(name,Keys.ENTER);
+        return this;
     }
 }

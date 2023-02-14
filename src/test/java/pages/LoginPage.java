e689d85242d9262c3910e0362f64890cd1fec8be
Kostiantyn <konstantin.pavlishin@testpro.io>
2023-02-14 15:27:21 -0600

    Update LoginPage.java

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,22 +1,41 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
+import java.time.Duration;
+
 public class LoginPage extends BasePage {
-    private final By emailFieldLocator = By.cssSelector("[type='email']");
-    private final By passwordFieldLocator= By.cssSelector("[type='password']");
-    private final By submitButtonLocator = By.cssSelector("button[type='submit']");
-    public void enterEmail(String email) {
-        click(emailFieldLocator);
-        sendKeys(emailFieldLocator,email);
+    @FindBy(css = "[type='email']")
+    WebElement emailFieldLocator;
+    @FindBy(css = "[type='password']")
+    WebElement passwordFieldLocator;
+    @FindBy(css = "button[type='submit']" )
+    WebElement submitButtonLocator;
+    //private final By emailFieldLocator = By.cssSelector("[type='email']");
+    //private final By passwordFieldLocator= By.cssSelector("[type='password']");
+    //private final By submitButtonLocator = By.cssSelector("button[type='submit']");
+
+    public LoginPage(WebDriver setDriver) {
+        super(setDriver);
     }
-    public void enterPassword(String password) {
-        click(passwordFieldLocator);
-        sendKeys(passwordFieldLocator,password);
+
+    public LoginPage enterEmail(String email) {
+        emailFieldLocator.click();
+        emailFieldLocator.sendKeys(email);
+        return this;
     }
-    public void loginSubmit() {
-        click(submitButtonLocator);
+    public LoginPage enterPassword(String password) {
+        passwordFieldLocator.click();
+        passwordFieldLocator.sendKeys(password);
+        return this;
+    }
+    public LoginPage loginSubmit() {
+        submitButtonLocator.click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+        return this;
     }
 }

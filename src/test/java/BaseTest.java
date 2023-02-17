52121b762447d9305ccfc324f73080db8c333688
JuliaBurkovaQA <julia.burkova@testpro.io>
2023-02-17 19:18:00 +0300

    Homework 24

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -2,11 +2,18 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import pageObject.LoginPage;
+
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.time.Duration;
 public class BaseTest {
     static WebDriver driver;
@@ -18,14 +25,38 @@ public class BaseTest {
     }
 
     @BeforeMethod
-    public void setUpBrowser() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-notifications");
-        driver = new ChromeDriver(options);
+    public void setUpBrowser() throws MalformedURLException {
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--disable-notifications");
+//        driver = new ChromeDriver(options);
+        driver = pickBrowser(System.getProperty("browser"));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         wait = new WebDriverWait(driver, Duration.ofSeconds(4));
        // driver.manage().window().maximize();
         driver.get(url);
+    }
+
+    public WebDriver pickBrowser(String browser) throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        String gridURL = "http://192.168.24.184:4444";
+        switch (browser) {
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver","geckodriver");
+                return driver = new FirefoxDriver();
+            case "edge":
+                return driver = new EdgeDriver();
+            case "grid-firefox":
+                capabilities.setCapability("browserName","firefox");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), capabilities);
+            case "grid-edge":
+                capabilities.setCapability("browserName","edge");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), capabilities);
+            case "grid-chrome":
+                capabilities.setCapability("browserName","chrome");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), capabilities);
+            default:
+                return driver = new ChromeDriver();
+        }
     }
 
     @AfterMethod

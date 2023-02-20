4351f1cfe7f19c48134448d3a7a288b2f42197bf
Kostiantyn <konstantin.pavlishin@testpro.io>
2023-02-19 19:48:12 -0600

    Update DeletePlaylist.java

--- src/test/java/DeletePlaylist.java
+++ src/test/java/DeletePlaylist.java
@@ -3,14 +3,14 @@ import org.testng.annotations.Test;
 import pages.HomePage;
 import pages.LoginPage;
 
-public class DeletePlaylist extends  BaseTest{
+public class DeletePlaylist extends  BaseTest {
     @Test
-    public void deletePlaylist()  {
-        LoginPage loginPage = new LoginPage(driver);
-        HomePage homePage = new HomePage(driver);
+    public void deletePlaylist() {
+        LoginPage loginPage = new LoginPage(getDriver());
+        HomePage homePage = new HomePage(getDriver());
         loginPage.enterEmail("rumkostia92@gmail.com")
-        .enterPassword("te$t$tudent")
-        .loginSubmit();
+                .enterPassword("te$t$tudent")
+                .loginSubmit();
         homePage.deleteActions();
         Assert.assertTrue(homePage.getNotification().isDisplayed());
     }

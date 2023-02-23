daf5e0a28941064d860a7c09866c263bcc5651fe
DESKTOP-3D86B1I\Ksenia Strizhkova <ksenia.strigkova@gmail.com>
2023-02-23 11:16:26 -0600

    Homework25

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -7,13 +7,56 @@ public class LoginTests extends BaseTest {
 
     @Test
     public void LoginSucceedTest() {
-        LoginPage loginPage = new LoginPage(getThreadDriver());
-        HomePage homePage = new HomePage(getThreadDriver());
+        LoginPage loginPage = new LoginPage(driver);
+        HomePage homePage = new HomePage(driver);
 
         loginPage.provideLoginSucceed();
 
         Assert.assertTrue(homePage.getUserAvatar().isDisplayed());
     }
 
+    @Test
+    public static void LoginEmptyEmailPasswordTest() {
+        LoginPage loginPage = new LoginPage(getThreadDriver());
 
+        loginPage.provideEmail("")
+                .providePassword("te$t$tudent")
+                .clickSubmitBtn();
+
+        Assert.assertTrue(loginPage.getRegistrationLink().isDisplayed());
+    }
+
+    @Test
+    public static void LoginWrongPasswordTest() {
+        LoginPage loginPage = new LoginPage(getThreadDriver());
+
+        loginPage.provideEmail("ksenia.strigkova@gmail.com")
+                .providePassword("te$t123")
+                .clickSubmitBtn();
+
+        Assert.assertTrue(loginPage.getRegistrationLink().isDisplayed());
+    }
+
+    @Test
+    public static void LoginEmptyPasswordTest() {
+        LoginPage loginPage = new LoginPage(getThreadDriver());
+
+        loginPage.provideEmail("ksenia.strigkova@gmail.com")
+                .providePassword("")
+                .clickSubmitBtn();
+
+        Assert.assertTrue(loginPage.getRegistrationLink().isDisplayed());
+    }
+
+    @Test
+    public static void LoginWrongEmailTest() {
+        LoginPage loginPage = new LoginPage(getThreadDriver());
+
+        loginPage.provideEmail("ksenia.strigkova@gmail.c")
+                .providePassword("te$t$tudent")
+                .clickSubmitBtn();
+
+        Assert.assertTrue(loginPage.getRegistrationLink().isDisplayed());
+    }
 }
+---

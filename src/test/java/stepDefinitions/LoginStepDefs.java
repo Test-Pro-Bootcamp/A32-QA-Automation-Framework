c6bb42a5ddd4ff195d057bbd1bf081cb3f4c71f2
goldcluster_LK <123644292+goldclusterLK@users.noreply.github.com>
2023-04-10 07:57:28 -0500

    HW27 ChromeDriver Updates

--- src/test/java/stepDefinitions/LoginStepDefs.java
+++ src/test/java/stepDefinitions/LoginStepDefs.java
@@ -9,7 +9,6 @@ import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import pages.LoginPage;
